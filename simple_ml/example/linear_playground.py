fix softmax.

simple_ml/example/linear_playground.py
@@ -104,16 +104,17 @@ def model_mlp_mnist():
 
     """
     from tensorflow.examples.tutorials.mnist import input_data
-    mnist = input_data.read_data_sets('./tmp/data', one_hot=True)
+    mnist = input_data.read_data_sets('./tmp/data', one_hot=False)
     training_data = np.array([image.flatten() for image in mnist.train.images])
     training_label = mnist.train.labels
     valid_data = np.array([image.flatten() for image in mnist.validation.images])
     valid_label = mnist.validation.labels
     input_dim = training_data.shape[1]
-    label_size = training_label.shape[1]
+    label_size = 10
 
     Inputs = Dense(300, input_dim=input_dim, activation=None)
-    X = Activation('relu')(Inputs)
+    # X = Activation('relu')(Inputs)
+    X = Activation('relu6')(Inputs)
     X = Dropout(0.2)(X)
     X = Softmax(label_size)(X)
     model = Model(Inputs, X)

simple_ml/nn/loss.py
@@ -65,11 +65,21 @@ class LogLikelihoodLoss(Loss):
 
     @staticmethod
     def forward(y_hat: np.array, y: np.array):
-        assert (np.abs(np.sum(y_hat, axis=1) - 1.) < cutoff).all()
-        assert (np.abs(np.sum(y, axis=1) - 1.) < cutoff).all()
+        # assert (np.abs(np.sum(y_hat, axis=1) - 1.) < cutoff).all()
+        # assert (np.abs(np.sum(y, axis=1) - 1.) < cutoff).all()
+        # print(y)
+        # print(y.shape)
+        # y_hat = _cutoff(y_hat)
+        # y = _cutoff(y)
+        # return -np.sum(np.sum(np.nan_to_num(y * np.log(y_hat)), axis=1), axis=0)
+        # print('y_hat size:', y_hat.shape)
+        # print('y size:', y.shape)
+        # exit()
+        y = y.astype(int)
         y_hat = _cutoff(y_hat)
-        y = _cutoff(y)
-        return -np.sum(np.sum(np.nan_to_num(y * np.log(y_hat)), axis=1), axis=0)
+        log_probs = np.log(y_hat)
+        batch_size = y_hat.shape[0]
+        return -np.sum(log_probs[np.arange(batch_size), y])
 
     @staticmethod
     def backward(y_hat: np.array, y: np.array):
@@ -81,11 +91,16 @@ class LogLikelihoodLoss(Loss):
         :param activator:
         :return:
         """
-        assert (np.abs(np.sum(y_hat, axis=1) - 1.) < cutoff).all()
-        assert (np.abs(np.sum(y, axis=1) - 1.) < cutoff).all()
+        # assert (np.abs(np.sum(y_hat, axis=1) - 1.) < cutoff).all()
+        # assert (np.abs(np.sum(y, axis=1) - 1.) < cutoff).all()
+        # y_hat = _cutoff(y_hat)
+        # y = _cutoff(y)
+        # return y_hat - y
+        y = y.astype(int)
         y_hat = _cutoff(y_hat)
-        y = _cutoff(y)
-        return y_hat - y
+        batch_size = y_hat.shape[0]
+        y_hat[np.arange(batch_size), y] -= 1
+        return y_hat
 
 
 # alias

simple_ml/nn/model.py
@@ -53,7 +53,8 @@ class Module(object):
             sample_y_pred = np.around(sample_y_pred, 2).squeeze()
             dtype = float
         elif peek_type == 'single-cls':
-            sample_y = np.argmax(sample_y, axis=1)
+            # sample_y = np.argmax(sample_y, axis=1)
+            sample_y = sample_y
             sample_y_pred = np.argmax(sample_y_pred, axis=1)
             dtype = int
         else:

simple_ml/nn/ops.py
@@ -78,17 +78,23 @@ def delta_identity(z):
 # softmax #
 ###########
 def softmax(z):
-    z = np.asarray(z)
-    if len(z.shape) > 1:
-        z -= np.sum(z, axis=1).reshape([z.shape[0], 1])
-        z = np.exp(z)
-        z /= np.sum(z, axis=1).reshape([z.shape[0], 1])
-        return z
-    else:
-        z -= np.max(z)
-        z = np.exp(z)
-        z /= np.sum(z)
-        return z
+    # z = np.asarray(z)
+    # if len(z.shape) > 1:
+    #     z -= np.sum(z, axis=1).reshape([z.shape[0], 1])
+    #     z = np.exp(z)
+    #     z /= np.sum(z, axis=1).reshape([z.shape[0], 1])
+    #     return z
+    # else:
+    #     z -= np.max(z)
+    #     z = np.exp(z)
+    #     z /= np.sum(z)
+    #     return z
+    z = np.asanyarray(z)
+    # shift
+    z -= np.max(z, axis=-1, keepdims=True)
+    z = np.exp(z)
+    z /= np.sum(z, axis=-1, keepdims=True)
+    return z
 
 
 def delta_softmax(z):

simple_ml/utils/metric.py
@@ -5,7 +5,8 @@ __all__ = ['accuracy', 'mean_absolute_error', 'mean_square_error']
 
 def accuracy(outputs, targets):
     y_predicts = np.argmax(outputs, axis=1)
-    y_targets = np.argmax(targets, axis=1)
+    # y_targets = np.argmax(targets, axis=1)
+    y_targets = targets
     acc = y_predicts == y_targets
     return np.sum(acc, axis=0)
 