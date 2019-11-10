eb90a4eeb08a572f43f6c1f972fa84e7fba8b578
lizhaoliu_Bak <13611425862llz@gmail.com>
2019-11-10 21:28:50 +0800

    complete exp1.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pip_setup',
-    version='0.1.0',
+    version='0.1.1',
     description='simple: simple machine learning framework.',
     classifiers=[
         'License :: OSI Approved :: MIT License',
